9578fc72ce4aa98cac880609e92906016e11d894
Sagnik Taraphdar <code.sagnik@gmail.com>
2026-06-01 17:51:03 +0530

    feat(vectorflux): export Node and make_* graph construction functions

--- python/vectorflux/__init__.py
+++ python/vectorflux/__init__.py
@@ -1,3 +1,17 @@
-from ._core import hello_cuda, Tensor, add, mul, relu, matmul
+from ._core import (
+    hello_cuda,
+    Tensor,
+    add, mul, relu, matmul,
+    Node,
+    make_const, make_add, make_mul, make_relu, make_matmul,
+    reset_default_graph,
+)
 
-__all__ = ["hello_cuda", "Tensor", "add", "mul", "relu", "matmul"]
+__all__ = [
+    "hello_cuda",
+    "Tensor",
+    "add", "mul", "relu", "matmul",
+    "Node",
+    "make_const", "make_add", "make_mul", "make_relu", "make_matmul",
+    "reset_default_graph",
+]
